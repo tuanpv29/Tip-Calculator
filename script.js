379e06f10583ed04efea39b65bc5d20f2ea346d6
unknown <tuanpv29@gmail.com>
2021-01-28 22:34:21 +0700

    Create Tip Calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+// Selectors
+const billAmount = document.getElementById("billamount");
+const tipPercentage = document.getElementById("tippercentage");
+const tipAmount = document.getElementById("tipamount");
+const totalBill = document.getElementById("totalbill");
+const calculateBtn = document.getElementById("calculate");
+
+// Event Listeners
+calculateBtn.addEventListener('click', sum);
+
+// Functions
+function sum(event) {
+  // Prevent form from submitting
+  event.preventDefault();
+
+  // Prevent empty input
+  if (billAmount.value == '') {
+    alert('you must to fill Bill Amount!');
+  };
+  if (tipPercentage.value == '') {
+    alert('you must to fill Tip Percentage!')
+  };
+  // Returns Bill Amount Input Value
+  const amount = Number(billAmount.value);
+  // Returns Bill Amount Input Value
+  const per = Number(tipPercentage.value);
+  // Calculate Tip Amount
+  const tip = amount * per / 100;
+  tipAmount.value = tip;
+  // Calculate Total Bill
+  const total = amount + tip;
+  totalBill.value = total;
+}
